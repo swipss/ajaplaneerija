d87416c2d0f9d5820e002751dc2e2265f41a1cd9
Heigins <112852877+Heigins@users.noreply.github.com>
2022-09-26 10:31:58 +0300

    Update Source code.py

--- src/Source code.py
+++ src/Source code.py
@@ -1,1 +1,11 @@
-print("Tere")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/avaleht')
+def hello_world():
+    return 'Tere, maailm!' 
+  
+if __name__ == '__main__':
+    app.run()
